f1a3bdce5bc69cc98c426581f9f17cd50fbccbb1
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-30 12:02:10 +0530

    changes

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,11 +9,12 @@ namespace MovieStoreApp.Models
     internal class Movie
     {
         public int Id { get; set; }
+        public int Age {get; set;}
         public string Name { get; set; }
         public int YearOfRelease {  get; set; }
         public string Genre { get; set; }
 
-        public Movie(){}
+        public MovieClass(){}
 
         public Movie(int id,string name,int year,string genre)
         {
@@ -21,13 +22,14 @@ namespace MovieStoreApp.Models
             Name = name;
             YearOfRelease = year;
             Genre = genre;
+            Age = age;
         }/*
         public override string ToString()
         {
             return $"Id:{Id}\nName:{Name}\nRelease Year:{YearOfRelease}\nGenre:{Genre}\n";
         }
         */
-        public static Movie AddNewMovie(int id,string name,int year,string genre)
+        public static Movie AddNewMovieOne(int id,string name,int year,string genre)
         {
             return new Movie(id,name,year,genre);
         }
